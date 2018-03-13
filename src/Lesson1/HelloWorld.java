8937a5756d3125ba02347e36871787a7a60bcb34
LiliiaZubkova <life7773@gmail.com>
2018-03-13 19:24:37 +0200

    committed initial commit

---
+++ src/Lesson1/HelloWorld.java
@@ -0,0 +1,20 @@
+package Lesson1;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, world");
+        boolean b = true;
+        char c = 'f';
+        int i = 2;
+        long l = 500000088L;
+        float f = 9.5f;
+        double d = 9.5;
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+
+    }
+}
