43c9ace61e07f46570205c6c87417fcacde71b06
LiliiaZubkova <life7773@gmail.com>
2018-03-20 16:48:21 +0200

    Hometask

---
+++ src/Lesson2/eachNth.java
@@ -0,0 +1,20 @@
+package Lesson2;
+
+import java.util.Scanner;
+
+public class Strings {
+
+    Scanner n = new Scanner(System.in);
+    System.out.println("Please, put any integer number: ");
+    int n = n.nextInt();
+    Scanner ln = new Scanner(System.in);
+    System.out.println("Please, put any string: ");
+    int ln = ln.nextInt();
+
+    public void eachNth(n, ln) {
+
+
+    }
+
+
+}
